1606325b5408af079980cdecd787bb6197311f5e
hina-raza <61477000+hina-raza@users.noreply.github.com>
2020-04-23 10:18:35 -0400

    created functionality to jump from login view to movies display

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {FormsModule} from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
@@ -12,7 +13,8 @@ import { MovieComponent } from './login/movies/movies.component';
     MovieComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
