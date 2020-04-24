1d50b05d565650eb83b713b77de3bc7e1f04bf7f
hina-raza <61477000+hina-raza@users.noreply.github.com>
2020-04-24 10:09:42 -0400

    This is a movie rating app with a login feaure and a movie display grid

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,31 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import {FormsModule} from '@angular/forms';
+import {HttpClientModule} from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { MovieComponent } from './login/movies/movies.component';
+import { AppFooter } from './footer/footer.component';
+import { AppHeader } from './header/header.component';
+import { AppMain } from './main/main.component';
+import { AppNav } from './nav/nav.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
-    MovieComponent
+    MovieComponent,
+    AppHeader,
+    AppFooter,
+    AppMain,
+    AppNav
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    HttpClientModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
